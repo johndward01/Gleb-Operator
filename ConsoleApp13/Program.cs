8fc4336e1fd7116f81e82977282f888457dd41bc
jdw <johndward01@gmail.com>
2022-01-11 15:29:08 -0600

    Add project files.

---
+++ ConsoleApp13/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace ConsoleApp13
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            string firstName = "John";
+            string lastName = "Ward";
+
+            // TODO: Print out to the console the first name and the last name
+            // inside of a string interpolation!! (Hint: Research string interpolation)
+            
+        }
+
+
+    }
+}
